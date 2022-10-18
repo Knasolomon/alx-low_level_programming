0e8cd7c8282ec6edbfbd2c58ab1f930873338df9
Knasolomon <Solomon.kevin152@gmail.com>
2022-10-17 20:30:12 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int number;
+	char letter;
+
+	for (number = 0; number < 10; number++)
+	{
+		putchar((number % 10) + '0');
+	}
+
+	for (letter = 'a'; letter <= 'z', letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
