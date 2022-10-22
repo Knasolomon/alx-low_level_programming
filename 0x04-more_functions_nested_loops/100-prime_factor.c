bb3146c7c3243fe18329d0d1fa74e2399787f327
Knasolomon <Solomon.kevin152@gmail.com>
2022-10-21 17:29:32 -0700

    Task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints the largest prime factor of the number 612852475143
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long a, b;
+
+	a = 612852475143;
+
+	for (b = 2; a > b; b++)
+	{
+		while (a % b == 0)
+		{
+			a = a / b;
+		}
+	}
+
+	printf("%lu", b);
+	putchar('\n');
+
+	return (0);
+}
