5a3cf8784741472c8a47796ce2e455ee08891143
Knasolomon <Solomon.kevin152@gmail.com>
2022-11-04 20:07:28 -0700

    Task 0

---
+++ 0x0A-agrc_agrv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the name of a program followed by a new line
+ * @argc: count arguments
+ * @argv: arguments
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
