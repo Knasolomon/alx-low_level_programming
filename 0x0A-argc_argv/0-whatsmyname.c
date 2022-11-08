b25fc054035d58f36fb5538d12ac757446a032d8
Knasolomon <Solomon.kevin152@gmail.com>
2022-11-08 04:51:39 -0800

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,9 @@
  * Return: Always 0
  */
 
-int main(int argc __atribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", *argv);
+	printf("%s\n", argv[argc * 0]);
 
 	return (0);
 }
